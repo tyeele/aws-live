bc0ea68307b70439a6655bcb3fd49cfac8fb7c4f
tyeele <56420582+tyeele@users.noreply.github.com>
2022-04-15 13:05:34 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.ckb1yoczeut2.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "cpac3009"
+customdb = "employee"
+custombucket = "teohyeele-employee"
+customregion = "us-east-1"
